7d46867cae470bd7bfcd7c41b494cc66d00bb392
Mario Aranda <mario.aranda2551@gmail.com>
2019-03-05 15:57:29 -0600

    deletion of file
    no longer needed register sw file

--- js/register-sw.js
+++
@@ -1,8 +0,0 @@
-// register the service worker for offline use
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./sw.js')
-    .then(function() {
-      console.log("Service Worker Registered");
-    });
-}
